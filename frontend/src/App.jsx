4b54e8a34b6213905ac90f52afdd557d8fe6357d
Fredrik Andrén <fredrik.andren98@gmail.com>
2023-03-20 16:54:58 +0100

    Configured the football-data.org api to the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom'
 import Loginscreen from './screens/Loginscreen/Loginscreen';
 import Landingscreen from './screens/Landingscreen/Landingscreen'
 import RegisterScreen from './screens/Registerscreen/Registerscreen'
+import Homescreen from './screens/Homescreen/Homescreen';
 const App =() => {
   return (
   <Routes>
     <Route path='/' element={<Landingscreen/>}/>
     <Route path='/login' element={<Loginscreen/>}/>
     <Route path='/register' element={<RegisterScreen/>}/>
+    <Route path='/homescreen' element={<Homescreen/>}/>
   </Routes>
   )
 }
--- frontend/src/screens/Homescreen/Homescreen.jsx
+++ frontend/src/screens/Homescreen/Homescreen.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 
 const Homescreen = () => {
+    const [premierLeague, setPremierLeague] = useState([])
+
+    useEffect(() => {
+    
+    axios.get('/api/leagues/', {
+      headers: {
+        'X-Auth-Token': '58d5d5351e7444a2815fcbb0b0a058b9' , 
+      },
+    })
+        .then(response => {
+            const premierLeagueData = response.data
+            setPremierLeague(premierLeagueData)
+        })
+        .catch(error => console.log(error))
+    }, [])
   return (
     <>
-    
+    <h1>Premier League Table</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Position</th>
+                <th>Team</th>
+                <th>Won</th>
+                <th>Drawn</th>
+                <th>Lost</th>
+                <th>GF</th>
+                <th>GA</th>
+                <th>GD</th>
+                <th>Points</th>
+            </tr>
+        </thead>
+        <tbody>
+  {premierLeague.map((team, index) => {
+    return (
+      <tr key={team.position}>
+        <td>{team.position}</td>
+        <img style={{width: '30px'}} src={team.team.crestUrl} alt={team.team.name} />
+        <td>{team.team.name}</td>
+        <td>{team.playedGames}</td>
+        <td>{team.won}</td>
+        <td>{team.draw}</td>
+        <td>{team.lost}</td>
+        <td>{team.goalsFor}</td>
+        <td>{team.goalsAgainst}</td>
+        <td>{team.goalDifference}</td>
+        <td>{team.points}</td>
+      </tr>
+    )
+  })}
+</tbody>
+
+    </table>
+
     </>
   )
 }
